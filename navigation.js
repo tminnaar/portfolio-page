60f28c52ca1637dfec9b0f1887e6d00e0c34b458
tminnaar <tminnaar94@gmail.com>
2023-01-19 01:36:20 +0000

    about slider animation

--- navigation.js
+++ navigation.js
@@ -1,33 +1,29 @@
 const aboutButton = document.getElementById("about-button");
 const projectsButton = document.getElementById("projects-button");
+const infoPanel = document.getElementById("info-panel");
 const projectSlider = document.getElementById("project-slider");
-const aboutMe = document.getElementById("about-info");
-
-// function navReveal() {
-// 	if ((projectSlider.style.display = "none")) {
-// 		projectSlider.style.display = "flex";
-// 		projectSlider.style.flexDirection = "column";
-// 		projectSlider.style.animationName;
-// 	}
-// }
+const aboutPanel = document.getElementById("about-panel");
+const aboutImage = document.getElementById("about-image");
 
 function toggleElements() {
 	aboutButton.addEventListener("click", function () {
-		aboutMe.style.display = "flex";
-		projectSlider.style.animation = "navSlideOut 3s forwards";
-		// projectSlider.style.display = "none";
+		aboutPanel.style.display = "flex";
+		aboutImage.style.display = "flex";
+		aboutPanel.style.animation = "aboutSlideIn 3s forwards";
+		projectSlider.style.animation = "navSlideOut 2s forwards";
 	});
 
 	projectsButton.addEventListener("click", function () {
-		aboutMe.style.display = "none";
 		projectSlider.style.display = "flex";
 		projectSlider.style.flexDirection = "column";
+		aboutPanel.style.animation = "aboutSlideOut 1.5s forwards";
 		projectSlider.style.animation = "navSlideIn 3s forwards";
 	});
 }
 
 function hideSliderOnLoad() {
 	projectSlider.style.display = "none";
+	aboutPanel.style.display = "none";
 }
 
 hideSliderOnLoad();
