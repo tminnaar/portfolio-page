c6bdde2530f194b70d97111dd6fb64b0558ca42c
tminnaar <tminnaar94@gmail.com>
2023-01-21 01:59:37 +0000

    info-panel restructure

--- navigation.js
+++ navigation.js
@@ -2,15 +2,19 @@ const aboutButton = document.getElementById("about-button");
 const projectsButton = document.getElementById("projects-button");
 const infoPanel = document.getElementById("info-panel");
 const projectSlider = document.getElementById("project-slider");
-const aboutPanel = document.getElementById("about-panel");
+const aboutPanel = document.getElementById("panel-about");
 const aboutImage = document.getElementById("about-image");
 
 function toggleElements() {
 	aboutButton.addEventListener("click", function () {
-		aboutPanel.style.display = "flex";
-		aboutImage.style.display = "flex";
+		aboutPanel.style.display = "block";
+		aboutImage.style.display = "block";
 		aboutPanel.style.animation = "aboutSlideIn 3s forwards";
 		projectSlider.style.animation = "navSlideOut 2s forwards";
+		projectsButton.style.display = "block";
+		// projectsButton.style.animation = "slideInFromBottom 2s forwards";
+		projectsButton.classList.add("buttonSlideUp");
+		aboutButton.classList.add("buttonSlideUp");
 	});
 
 	projectsButton.addEventListener("click", function () {
